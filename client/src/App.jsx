1ffc48ad1069abdb88d7b96a91352b14c73fff81
Sauby <imsauby@gmail.com>
2024-02-23 22:02:27 +0530

    implemented routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { HomePage } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
